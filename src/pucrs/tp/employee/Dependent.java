9edb7c79c122d77678e60a5288073da5486a5e2e
lukzfreitas <lucasrgs17@gmail.com>
2016-08-14 20:42:54 -0300

    adicionando método para calculo de idade para a classe Depedent.java

--- src/pucrs/tp/employee/Dependent.java
+++ src/pucrs/tp/employee/Dependent.java
@@ -1,7 +1,12 @@
 package pucrs.tp.employee;
 
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Dependent extends Person{
 	private Date dateBirth;
+	private int age;
 
 	public Dependent(String name, Date dateBirth) throws IllegalArgumentException {
 		super(name);
@@ -15,8 +20,14 @@ public class Dependent extends Person{
 		return (dateBirth);
 	}
 
-	public int age(int currentYear) {
-		return (currentYear = dateBirth.getYear());
+	public void calculateAge() {
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		this.age = year - dateBirth.getYear();
+	}
+
+	public int getAge() {
+		return this.age;
 	}
 
 	public String toString() {
